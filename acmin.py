bc4120d4974d9c2bd0ffee6ff82d54575e73ae36
Tuniubfav <jy923803@gmail.com>
2020-07-19 13:00:24 -0500

    Building Bot

---
+++ acmin.py
@@ -0,0 +1,10 @@
+import discord
+import os
+
+client = discord.Client()
+
+@client.event
+async def on_ready():
+    print("Bot encendido")
+
+client.run("NzMyNDI5MTg5MjM5NjY4OTE4.Xw0eCg.umnrxesYMBnMODD4yJCFKuQx484")
